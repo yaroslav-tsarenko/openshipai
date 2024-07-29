e4b6f42b762f453ccf87314a3a872fb8d112e6da
Yaroslav <yaroslav7v@gmail.com>
2024-07-29 23:00:14 +0300

    changed constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
-export const BACKEND_URL = 'http://localhost:8080';
+export const BACKEND_URL = 'https://openshipai-backend.onrender.com';
 export const FRONTEND_URL = 'https://openshipai.onrender.com';
-export const SOCKET_URL = 'https://socket-chat-server-xly7.onrender.com';
+export const SOCKET_URL = 'https://openshipai-assistant-server.onrender.com';
 export const ASSISTANT_URL = 'http://localhost:7777';
 
 
