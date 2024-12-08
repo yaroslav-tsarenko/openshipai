770d5cdb0bb0abffe2681947217f6d03099409b7
Yaroslav <yaroslav7v@gmail.com>
2024-12-08 16:58:57 +0200

    optimized svgs

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,16 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'assets/',
+                    },
+                },
             }
         ]
     },
