2935317d5eee2f46047630ce0b8ad6016010fe01
Yaroslav <yaroslav7v@gmail.com>
2024-12-23 15:34:15 +0200

    header changes, bug fixes, improvements

--- src/components/landing-page-new/header/Header.jsx
+++ src/components/landing-page-new/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Header.module.scss';
 import { ReactComponent as Logo } from '../../../assets/images/openshipai.svg';
 import  Logo2 from '../../../assets/images/openshipai.svg';
@@ -7,6 +7,21 @@ import { ReactComponent as FaTimes } from '../../../assets/images/fa-times-icon.
 import { Link } from "react-router-dom";
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
@@ -56,10 +71,12 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-            <header className={styles.header2}>
+            <header
+                className={`${styles.header2} ${isScrolled ? styles.header2Scrolled : ''}`}
+            >
                 <div className={styles.headerInner2}>
                     <Link to="/">
-                        <img src={Logo2} className={styles.logo2}/>
+                        <img src={Logo2} className={styles.logo2} alt={"image"}/>
                     </Link>
                     <nav className={styles.navContent}>
                         <Link to={"/open-page"} className={styles.navLink}>Open</Link>
